ee1ad0391bb8b322f1cd6db0f07ebb931a0009f2
VictorKengoo <victor.kengoo.kodama@accenture.com>
2020-06-04 04:38:17 -0300

    Adição de recursos GET e POST

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,53 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade  estamos acessndo do sistema
+
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação existente no back-end
+// DELETE: Remover uma informação do back-end
+
+// POST http://localhost:3333/users = Criar um usuário
+// GET http://localhost:3333/users = Listar usuários
+// GET http://localhost:3333/users/5 = Buscar dados do usuário com Id = 5
+
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros opcionais que vem na própria rota, para filtro, paginação
+// Request Body: Parâmetros para criação/ atualização de informações
+
+const users = ["Victor", "Diego", "Cleiton"];
+
 app.get("/users", (request, response) => {
-  console.log("Listagem de usuários");
+  const search = String(request.query.search);
 
-  //JSON
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
 
-  response.json(["Victor", "Diego", "Cleiton"]);
+  return response.json(filteredUsers);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
