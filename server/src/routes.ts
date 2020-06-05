5a64a474f7cce54341435f52c2b1bb3a973bee4b
VictorKengoo <victor.kengoo.kodama@accenture.com>
2020-06-05 04:18:25 -0300

    Configurações DB

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -24,6 +24,7 @@ routes.get("/items", async (request, response) => {
 
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: "http://localhost:3333/uploads/" + item.image,
     };
@@ -32,33 +33,39 @@ routes.get("/items", async (request, response) => {
   return response.json(serializedItems);
 });
 
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const ids = await knex("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    };
+  });
+
+  await knex("point_items").insert(pointItems);
+
+  return response.json({ success: true });
+});
+
 export default routes;
-
-/*app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  return response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});*/
