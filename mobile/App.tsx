92ca6b91e7a66fc662d15124b6dc111b2dae858f
VictorKengoo <victor.kengoo.kodama@accenture.com>
2020-06-22 01:41:20 -0300

    Add map page

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import Routes from "./src/routes";
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
+import { AppLoading } from "expo";
+
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      ></StatusBar>
+      <Routes />
+    </>
+  );
+};
+
+export default App;
