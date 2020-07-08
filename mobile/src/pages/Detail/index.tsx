6b974784b804164f6be121a11e444c69e29b7ea6
VictorKengoo <victor.kengoo.kodama@accenture.com>
2020-07-08 02:06:25 -0300

    adaptação image_url

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -22,6 +22,7 @@ interface Params {
 interface Data {
   point: {
     image: string;
+    image_url: string;
     name: string;
     email: string;
     whatsapp: string;
@@ -77,7 +78,7 @@ const Detail = () => {
         <Image
           style={styles.pointImage}
           source={{
-            uri: data.point.image,
+            uri: data.point.image_url,
           }}
         />
         <Text style={styles.pointName}>{data.point.name}</Text>
